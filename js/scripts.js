c373dadda71bde77e9a7b6b3e02a30a97a08e0b5
luigi970 <luis_devesa@hotmail.com>
2021-05-23 09:14:19 -0300

    algunos ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,61 @@
-window.onload = function (){
-    var numPage = 1
-    var page = []
-    for(let i = 0; i < 4; i++) {
-        //numPage + i;
-        page.push(numPage + i);
-    }
-
-    page.forEach((pag) => {
-        fetch('content-page-' + pag + '.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('.page-' + pag).innerHTML = data;
-        });
-    })
-
+let pages = []
+for(let i = 1; i < 5; i++) {
+    pages.push(i);
 }
+
+
+pages.forEach((pag) => {
+  // Crea las divs para contener los html para usar con swipe
+  let div = document.createElement('div');
+  div.classList.add('page-' + pag);
+  document.querySelector(".swipe-wrap").appendChild(div)
+
+  // Conexion con archivos html
+  async function fetchConexion() {
+    const conexion = await fetch('content-page-' + pag + '.html');
+    const response = await conexion.text();
+    return response;
+  }
+  fetchConexion().then(data => {
+    document.querySelector('.page-' + pag).innerHTML = data;
+  }).catch(error => {
+    console.log('Error', error);
+  })
+});
+
+/* pages.forEach((pag) => {
+    fetch('content-page-' + pag + '.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('.page-' + pag).innerHTML = data;
+    });
+}) */
+
+/* pages.forEach((pag) => {
+  async function fetchConexion() {
+    const conexion = await fetch('content-page-' + pag + '.html');
+    const response = await conexion.text();
+    return response;
+  }
+  fetchConexion().then(data => {
+    document.querySelector('.page-' + pag).innerHTML = data;
+  }).catch(error => {
+    console.log('Error', error);
+  })
+}) */
+
+/*******/
+
+
+/* function addElement() {
+  const arrayPagina = ['page-1','page-2','page-3','page-4'];
+  arrayPagina.forEach((pag) => {
+      let div = document.createElement('div');
+      div.classList.add(pag);
+      document.querySelector(".swipe-wrap").appendChild(div)
+  });
+}
+document.body.onload = addElement; */
 
 
 /* fetch('content-page-' + numPage + '.html')
